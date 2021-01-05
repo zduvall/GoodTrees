ceb0145cbd73b22613eb2e36d92c1ba69bbf584d
Gabriel <gabriel.gutz@live.com>
2021-01-05 15:57:59 -0500

    Prep for merge with main

--- routes/trees.js
+++ routes/trees.js
@@ -11,21 +11,21 @@ const { csrfProtection, asyncHandler } = require("./utils");
 var router = express.Router();
 
 
-//GET all trees
-router.get('/', asyncHandler(async (req, res) => {
-    const trees = await db.Tree.findAll({ order: [["createdAt", 'DSC']] });
-    // console.log(trees);
-    res.render('Trees/trees-page', { trees })
-}));
+// //GET all trees
+// router.get('/', asyncHandler(async (req, res) => {
+//     const trees = await db.Tree.findAll({ order: [["createdAt", 'DSC']] });
+//     // console.log(trees);
+//     res.render('Trees/trees-page', { trees })
+// }));
 
-//GET a specific tree
-router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
-    const treeId = parseInt(req.params.id, 10);
+// //GET a specific tree
+// router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
+//     const treeId = parseInt(req.params.id, 10);
 
-    const tree = await db.Tree.findByPk(treeId);
+//     const tree = await db.Tree.findByPk(treeId);
 
-    res.render('Trees/specific-tree', { tree });
-}));
+//     res.render('Trees/specific-tree', { tree });
+// }));
 
 
 module.exports = router
