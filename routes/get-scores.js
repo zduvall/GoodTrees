984ee4abd1c90c0c7dc42c6145510c39e7f66588
Zachary <duvallzt@gmail.com>
2021-01-05 18:42:22 -0800

    Refactor getClimberScore

--- routes/get-scores.js
+++ routes/get-scores.js
@@ -15,13 +15,14 @@ const getTreeAvgScore = (tree, attribute) => {
 // get user's climber score (pass in user's climbedTrees and make sure to include all reviews on those trees)
 const getClimberScore = (climbedTrees) => {
   if (!climbedTrees.length) return "No trees climbed yet!!!"
+  
   let sum = 0;
-
+  
   climbedTrees.forEach(tree => {
     sum += getTreeAvgScore(tree, "difficulty");
   })
 
-  return sum / climbedTrees.length;
+  return sum;
 }
 
 module.exports = {
