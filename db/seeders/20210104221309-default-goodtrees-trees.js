49e9b61f79396cc0ad7bc50bd13249fbb5e61e88
Zachary <duvallzt@gmail.com>
2021-01-05 22:41:20 -0800

    Added more seeding data

--- db/seeders/20210104221309-default-goodtrees-trees.js
+++ db/seeders/20210104221309-default-goodtrees-trees.js
@@ -12,21 +12,39 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
-   return queryInterface.bulkInsert('Trees', [
-     {name: 'GrandOld', cityState:'San Francisco, CA', detLocation: 'right on top of the granderson hill',
-     description: 'A hundred year old Oak Tree about 42ft tall with amazing branches for climbing',
-     adderId: 1, createdAt: new Date(), updatedAt: new Date()
-    },
-    {name: 'SleepyHollow', cityState:'St Louis, Missouri', detLocation: 'pass the public graveyard',
-     description: 'Creepy, dark, 50 year old haunted tree ',
-     adderId: 2, createdAt: new Date(), updatedAt: new Date()
-    },
-    {name: 'ChuckNorris', cityState:'Seattle, WA', detLocation: '50 yards away from city hall',
-     description: 'Right next to police station, risky, but lots of branches',
-     adderId: 3, createdAt: new Date(), updatedAt: new Date()
-    },
-   ])
-},
+    return queryInterface.bulkInsert('Trees', [
+      {
+        name: 'GrandOld', cityState: 'San Francisco, CA', detLocation: 'Right on top of the granderson hill.',
+        description: 'A hundred year old Oak Tree about 42ft tall with amazing branches for climbing',
+        adderId: 1, createdAt: new Date(), updatedAt: new Date()
+      },
+      {
+        name: 'SleepyHollow', cityState: 'St Louis, MO', detLocation: 'Pass the public graveyard.',
+        description: 'Creepy, dark, 50 year old haunted tree ',
+        adderId: 2, createdAt: new Date(), updatedAt: new Date()
+      },
+      {
+        name: 'ChuckNorris', cityState: 'Seattle, WA', detLocation: '50 yards away from city hall.',
+        description: 'Right next to police station, risky, but lots of branches',
+        adderId: 3, createdAt: new Date(), updatedAt: new Date()
+      },
+      {
+        name: 'Treebeard', cityState: 'Fangorn, OR', detLocation: "On the Pechanga Reservation.",
+        description: 'He is an Ent and is said by Gandalf to be "the oldest living thing that still walks beneath the Sun upon this Middle-earth."',
+        adderId: 1, createdAt: new Date(), updatedAt: new Date()
+      },
+      {
+        name: 'The Great Oak', cityState: 'Temecula, CA', detLocation: 'Since he\'s able to move, he\'s not frequently in the same place, but he can\'t move too fast so keep your eyes peeled.',
+        description: 'The Great Oak, known as Wi’áaşal by Pechanga people, is recognized as the largest naturally grown indigenous coast live oak (Quercus agrifolia) in the Western United States.',
+        adderId: 2, createdAt: new Date(), updatedAt: new Date()
+      },
+      {
+        name: 'Grand Hangover', cityState: 'Grand Canyon Village, AZ', detLocation: 'Hanging over the edge of the tree near Yavapai Point',
+        description: 'People only climb this tree hanging over the edge if they\'re drunk...',
+        adderId: 3, createdAt: new Date(), updatedAt: new Date()
+      },
+    ])
+  },
 
   down: (queryInterface, Sequelize) => {
     /*
@@ -36,6 +54,6 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('People', null, {});
     */
-   return queryInterface.bulkDelete('Trees', null, {});
+    return queryInterface.bulkDelete('Trees', null, {});
   }
 };
--- db/seeders/20210104223636-default-goodtrees-reviews.js
+++ db/seeders/20210104223636-default-goodtrees-reviews.js
@@ -12,24 +12,43 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
-   return queryInterface.bulkInsert('Reviews', [
-     {reviewText: 'awesome tree, easy climb, pleasant view from top',
-    treeId: 1, reviewerId: 1, difficulty: 1, funFactor: 2, viewFromTop: 3,
-    createdAt: new Date(), updatedAt: new Date()
-  },
-  {reviewText: 'dont climb, feel like i\'ve been haunted now for life, also had to run from ghosts',
-  treeId: 2, reviewerId: 2, difficulty: 2, funFactor: 1, viewFromTop: 1,
-  createdAt: new Date(), updatedAt: new Date()
-},
-{reviewText: 'was chased by police after being warned by an employer of city hall',
-    treeId: 3, reviewerId: 3, difficulty: 3, funFactor: 4, viewFromTop: 4,
-    createdAt: new Date(), updatedAt: new Date()
-  },
-  {reviewText: 'climbed it in morning, low hanging branches allow for pleasant climb, great view',
-    treeId: 3, reviewerId: 1, difficulty: 2, funFactor: 3, viewFromTop: 4,
-    createdAt: new Date(), updatedAt: new Date()
-  }
-   ])
+    return queryInterface.bulkInsert('Reviews', [
+      {
+        reviewText: 'awesome tree, easy climb, pleasant view from top',
+        treeId: 1, reviewerId: 1, difficulty: 1, funFactor: 2, viewFromTop: 3,
+        createdAt: new Date(), updatedAt: new Date()
+      },
+      {
+        reviewText: 'dont climb, feel like i\'ve been haunted now for life, also had to run from ghosts',
+        treeId: 2, reviewerId: 2, difficulty: 2, funFactor: 1, viewFromTop: 1,
+        createdAt: new Date(), updatedAt: new Date()
+      },
+      {
+        reviewText: 'was chased by police after being warned by an employer of city hall',
+        treeId: 3, reviewerId: 1, difficulty: 3, funFactor: 4, viewFromTop: 4,
+        createdAt: new Date(), updatedAt: new Date()
+      },
+      {
+        reviewText: 'climbed it in morning, low hanging branches allow for pleasant climb, great view',
+        treeId: 3, reviewerId: 1, difficulty: 2, funFactor: 3, viewFromTop: 4,
+        createdAt: new Date(), updatedAt: new Date()
+      },
+      {
+        reviewText: 'had a pleasant conversation with Treebeard while I climbed him',
+        treeId: 4, reviewerId: 1, difficulty: 2, funFactor: 4, viewFromTop: 4,
+        createdAt: new Date(), updatedAt: new Date()
+      },
+      {
+        reviewText: 'I climbed for hours and there\'s still so much to explore',
+        treeId: 5, reviewerId: 1, difficulty: 1, funFactor: 4, viewFromTop: 2,
+        createdAt: new Date(), updatedAt: new Date()
+      },
+      {
+        reviewText: 'I\'m glad I survived!',
+        treeId: 6, reviewerId: 1, difficulty: 4, funFactor: 2, viewFromTop: 4,
+        createdAt: new Date(), updatedAt: new Date()
+      },
+    ])
   },
 
   down: (queryInterface, Sequelize) => {
@@ -40,6 +59,6 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('People', null, {});
       */
-   return queryInterface.bulkDelete('Reviews', null, {});
+    return queryInterface.bulkDelete('Reviews', null, {});
   }
 };
