83e3c964ebc5f7268e9d395a7a786855522dce92
Zachary <duvallzt@gmail.com>
2021-01-08 10:14:37 -0800

    Merging to checkout main

--- public/javascripts/specific-tree.js
+++ public/javascripts/specific-tree.js
@@ -7,4 +7,20 @@ document.addEventListener("DOMContentLoaded", async () => {
   const markClimbedBtn = document.getElementById('mark-climbed-btn')
   const markWantToClimbBtn = document.getElementById('mark-climbed-btn')
 
+  markClimbedBtn.addEventListener('click', async (event) => {
+  console.log(markClimbedBtn);
+    //   const body = {
+  //     climbStatus: true,
+  //     favStatus: false,
+  //     userId: locals.curUser.id,
+  //     treeId: tree.id,
+  //   }
+  //   const response = await fetch('/forestconnections', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(body)
+  //   }).then(res => res.json())
+  });
 });
