236929bc87df7471ebe4ce049874d788b1024e54
Zachary <duvallzt@gmail.com>
2021-01-08 09:03:32 -0800

    Change page name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index");
+  res.render("splash");
 });
 
 module.exports = router;
