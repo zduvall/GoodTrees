c2dd3b242aa8f5c6f9e5bc4dbf4d274aef1881e3
Zachary <duvallzt@gmail.com>
2021-01-08 11:17:55 -0800

    Make both buttons work

--- public/javascripts/specific-tree.js
+++ public/javascripts/specific-tree.js
@@ -1,57 +1,22 @@
-// asyncHandler
-const asyncHandler = (handler) => (req, res, next) =>
-  handler(req, res, next).catch((err) => next(err));
-
 document.addEventListener("DOMContentLoaded", async () => {
+
+  // asyncHandler
+  const asyncHandler = (handler) => (req, res, next) =>
+    handler(req, res, next).catch((err) => next(err));
+
   window.addEventListener("load", (event) => {
-    console.log("hello from javascript!");
+    console.log("hello from javascript!")
   });
 
   const markClimbedBtn = document.getElementById('mark-climbed-btn')
-  const markWantToClimbBtn = document.getElementById('mark-climbed-btn')
+  const markWantToClimbBtn = document.getElementById('mark-want-to-climb-btn')
 
   const userId = markClimbedBtn.getAttribute('data-userId')
   const treeId = markClimbedBtn.getAttribute('data-treeId')
 
-  // const createOrUpdateFC = async function (trueOrFalse) {
-  //   const body = {
-  //     climbStatus: trueOrFalse,
-  //     userId: userId,
-  //     treeId: treeId,
-  //   }
-  //   const response = await fetch('/forestconnections', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(body)
-  //   }).then(res => res.json())
-  // }
-
-  // markClimbedBtn.addEventListener('click', asyncHandler(async (event) => {
-  //   await createOrUpdateFC(true)
-  // }));
-
-  // markWantToClimbBtn.addEventListener('click', asyncHandler(async (event) => {
-  //   await createOrUpdateFC(false)
-  // }));
-  markClimbedBtn.addEventListener('click', asyncHandler(async (event) => {
+  const createOrUpdateFC = async function (trueOrFalse) {
     const body = {
-      climbStatus: true,
-      userId: userId,
-      treeId: treeId,
-    }
-    const response = await fetch('/forestconnections', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body)
-    }).then(res => res.json())  }));
-
-  markWantToClimbBtn.addEventListener('click', asyncHandler(async (event) => {
-    const body = {
-      climbStatus: false,
+      climbStatus: trueOrFalse,
       userId: userId,
       treeId: treeId,
     }
@@ -62,6 +27,15 @@ document.addEventListener("DOMContentLoaded", async () => {
       },
       body: JSON.stringify(body)
     }).then(res => res.json())
+  }
+  
+  markClimbedBtn.addEventListener('click', asyncHandler(async (event) => {
+    await createOrUpdateFC(true)
   }));
-
+  
+  markWantToClimbBtn.addEventListener('click', asyncHandler(async (event) => {
+    await createOrUpdateFC(false)
+  }));
+  
 });
+
