9fba4ee1e5d0ee63da0e6895a5abee2081e5752a
Zachary <duvallzt@gmail.com>
2021-03-29 14:55:09 -0600

    Complete some of delete a tree

--- public/javascripts/specific-tree.js
+++ public/javascripts/specific-tree.js
@@ -1,58 +1,84 @@
-document.addEventListener("DOMContentLoaded", async () => {
-
+document.addEventListener('DOMContentLoaded', async () => {
   // asyncHandler
   const asyncHandler = (handler) => (req, res, next) =>
     handler(req, res, next).catch((err) => next(err));
 
-  const markClimbedBtn = document.getElementById('mark-climbed-btn')
-  const markWantToClimbBtn = document.getElementById('mark-want-to-climb-btn')
-  const markRemoveBtn = document.getElementById('mark-remove')
+  // code for forest connections
+  const markClimbedBtn = document.getElementById('mark-climbed-btn');
+  const markWantToClimbBtn = document.getElementById('mark-want-to-climb-btn');
+  const markRemoveBtn = document.getElementById('mark-remove');
 
-  const userId = markClimbedBtn.getAttribute('data-userId')
-  const treeId = markClimbedBtn.getAttribute('data-treeId')
+  const userId = markClimbedBtn.getAttribute('data-userId');
+  const treeId = markClimbedBtn.getAttribute('data-treeId');
 
   const createOrUpdateFC = async function (trueOrFalse, deleteOrNot) {
     const body = {
       climbStatus: trueOrFalse,
       userId: userId,
       treeId: treeId,
-      deleteFC: deleteOrNot
-    }
+      deleteFC: deleteOrNot,
+    };
     const response = await fetch('/forestconnections', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(body)
-    }).then(res => res.json())
-  }
-  
-  markClimbedBtn.addEventListener('click', asyncHandler(async (event) => {
-    await createOrUpdateFC(true, false)
-    markClimbedBtn.disabled = true;
-    markClimbedBtn.innerHTML = 'Climbed ✓';
-    markWantToClimbBtn.disabled = false;
-    markWantToClimbBtn.innerHTML = 'Want to';
-    markRemoveBtn.disabled = false;
-  }));
-  
-  markWantToClimbBtn.addEventListener('click', asyncHandler(async (event) => {
-    await createOrUpdateFC(false, false);
-    markWantToClimbBtn.disabled = true;
-    markWantToClimbBtn.innerHTML = 'Want to ✓';
-    markClimbedBtn.disabled = false;
-    markClimbedBtn.innerHTML = 'Climbed';
-    markRemoveBtn.disabled = false;
-  }));
+      body: JSON.stringify(body),
+    }).then((res) => res.json());
+  };
 
-  markRemoveBtn.addEventListener('click', asyncHandler(async (event) => {
-    await createOrUpdateFC(false, true);
-    markWantToClimbBtn.disabled = false;
-    markWantToClimbBtn.innerHTML = 'Want to';
-    markClimbedBtn.disabled = false;
-    markClimbedBtn.innerHTML = 'Climbed';
-    markRemoveBtn.disabled = true;
-  }));
-  
+  markClimbedBtn.addEventListener(
+    'click',
+    asyncHandler(async (event) => {
+      await createOrUpdateFC(true, false);
+      markClimbedBtn.disabled = true;
+      markClimbedBtn.innerHTML = 'Climbed ✓';
+      markWantToClimbBtn.disabled = false;
+      markWantToClimbBtn.innerHTML = 'Want to';
+      markRemoveBtn.disabled = false;
+    })
+  );
+
+  markWantToClimbBtn.addEventListener(
+    'click',
+    asyncHandler(async (event) => {
+      await createOrUpdateFC(false, false);
+      markWantToClimbBtn.disabled = true;
+      markWantToClimbBtn.innerHTML = 'Want to ✓';
+      markClimbedBtn.disabled = false;
+      markClimbedBtn.innerHTML = 'Climbed';
+      markRemoveBtn.disabled = false;
+    })
+  );
+
+  markRemoveBtn.addEventListener(
+    'click',
+    asyncHandler(async (event) => {
+      await createOrUpdateFC(false, true);
+      markWantToClimbBtn.disabled = false;
+      markWantToClimbBtn.innerHTML = 'Want to';
+      markClimbedBtn.disabled = false;
+      markClimbedBtn.innerHTML = 'Climbed';
+      markRemoveBtn.disabled = true;
+    })
+  );
+
+  // code for delete
+  const deleteTreeBtn = document.getElementById('delete-tree');
+  const treeId = deleteTreeBtn.getAttribute('data-treeId');
+
+  deleteTreeBtn.addEventListener(
+    'click',
+    asyncHandler(async (event) => {
+      console.log('clicked!!!!');
+
+      const response = await fetch('/trees/delete', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(treeId),
+      }).then((res) => res.json());
+    })
+  );
 });
-
--- routes/trees.js
+++ routes/trees.js
@@ -1,90 +1,97 @@
-const express = require("express");
-const bcrypt = require("bcryptjs");
-const { check, validationResult } = require("express-validator");
-const { requireAuth } = require("../auth.js");
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+const { requireAuth } = require('../auth.js');
 
-const db = require("../db/models");
-const { getTreeAvgScore } = require("./get-scores");
+const db = require('../db/models');
+const { getTreeAvgScore } = require('./get-scores');
 
 const {
   csrfProtection,
   asyncHandler,
   createTreeValidators,
-} = require("./utils");
+} = require('./utils');
 
 const router = express.Router();
 
 //GET all trees
 router.get(
-  "/",
+  '/',
   requireAuth,
   asyncHandler(async (req, res) => {
     const trees = await db.Tree.findAll({
-      include: ["user"],
-      order: [["createdAt", "DESC"]],
+      include: ['user'],
+      order: [['createdAt', 'DESC']],
     });
-    res.render("Trees/trees-page", { trees });
+    res.render('Trees/trees-page', { trees });
   })
 );
 
 //GET a specific tree
 router.get(
-  "/:id(\\d+)",
+  '/:id(\\d+)',
   requireAuth,
   asyncHandler(async (req, res) => {
     const treeId = parseInt(req.params.id, 10);
-
     const tree = await db.Tree.findByPk(treeId, {
       include: [
         {
           model: db.Review,
-          as: "reviews",
+          as: 'reviews',
           include: {
             model: db.User,
-            as: "reviewer",
+            as: 'reviewer',
           },
         },
         {
           model: db.User,
-          as: "user",
+          as: 'user',
         },
       ],
     });
 
-    const avgDiff = getTreeAvgScore(tree, "difficulty");
-    const avgFun = getTreeAvgScore(tree, "funFactor");
-    const avgView = getTreeAvgScore(tree, "viewFromTop");
+    const avgDiff = getTreeAvgScore(tree, 'difficulty');
+    const avgFun = getTreeAvgScore(tree, 'funFactor');
+    const avgView = getTreeAvgScore(tree, 'viewFromTop');
 
     const existingFC = await db.ForestConnection.findOne({
       where: {
         userId: res.locals.curUser.id,
-        treeId
-      }
-    }); 
+        treeId,
+      },
+    });
 
     const existingReview = await db.Review.findOne({
       where: {
         treeId,
-        reviewerId: res.locals.curUser.id
-      }
+        reviewerId: res.locals.curUser.id,
+      },
     });
 
-    res.render("Trees/specific-tree", { tree, avgDiff, avgFun, avgView, existingFC, existingReview });
+    res.render('Trees/specific-tree', {
+      tree,
+      avgDiff,
+      avgFun,
+      avgView,
+      existingFC,
+      existingReview,
+      curUser: res.locals.curUser,
+    });
   })
 );
 
 //Create Tree
-router.get("/new", csrfProtection, requireAuth, (req, res) => {
+router.get('/new', csrfProtection, requireAuth, (req, res) => {
   const tree = db.Tree.build();
-  res.render("Trees/create-tree", {
-    title: "Create A New Tree",
+  res.render('Trees/create-tree', {
+    title: 'Create A New Tree',
     tree,
     csrfToken: req.csrfToken(),
   });
 });
 
 router.post(
-  "/new",
+  '/new',
   csrfProtection,
   createTreeValidators,
   requireAuth,
@@ -105,8 +112,8 @@ router.post(
       return res.redirect(`/trees/${tree.id}`);
     } else {
       const errors = validatorErrors.array().map((error) => error.msg);
-      res.render("Trees/create-tree", {
-        title: "Create A New Tree",
+      res.render('Trees/create-tree', {
+        title: 'Create A New Tree',
         tree,
         errors,
         csrfToken: req.csrfToken(),
@@ -114,4 +121,29 @@ router.post(
     }
   })
 );
+
+// delete tree
+router.post(
+  '/delete',
+  // csrfProtection,
+  requireAuth,
+  asyncHandler(async (req, res) => {
+    console.log('----------------- hit delete route!!! -----------------');
+
+    // const treeId = parseInt(req.params.treeId, 10);
+
+    // const treeToDelete = await db.Tree.findOne({
+    //   where: {
+    //     treeId,
+    //   },
+    // });
+
+    // console.log(treeToDelete.name);
+    // await treeToDelete.destroy();
+
+    // res.redirect(`/users/${res.locals.curUser.id}`);
+    return;
+  })
+);
+
 module.exports = router;
